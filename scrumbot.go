76f72091f9ea6b4577c4834a69f7b71d030c1e1b
Petr Karmashev <smonkl@bk.ru>
2017-08-06 01:49:41 +0300

    Slack bot for scrum mastering

---
+++ scrumbot.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "fmt"
+    "os"
+
+    "github.com/nlopes/slack"
+    "time"
+)
+
+const SLACK_TOKEN_ENVIRONMENT = "IQOPTION_SLACK_TOKEN_SCRUM_BOT"
+const BOT_NAME = "scrum"
+const CHANNEL_BOTTESTING = "G6J5NPD4Z"
+const CHANNEL_AFFILIATE_DEV = "C0FTH89AT"
+
+func main() {
+    api := slack.New(os.Getenv(SLACK_TOKEN_ENVIRONMENT))
+
+    groups, err := api.GetGroups(false)
+
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println("I'm now available in groups:")
+    for _, group := range groups {
+        fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
+    }
+
+    fmt.Println("")
+
+    ticker := time.NewTicker(1 * time.Second)
+
+    for t := range ticker.C {
+        fmt.Println("Tick at", t)
+
+        channelId, timestamp, err := api.PostMessage(
+            CHANNEL_BOTTESTING, getMeetingInvitation(),
+            slack.PostMessageParameters{
+                Username: BOT_NAME,
+                AsUser:   true,
+            },
+        )
+
+        if err != nil {
+            fmt.Printf("%s\n", err)
+            return
+        }
+
+        fmt.Printf("Message successfully sent to channel %s at %s\n", channelId, timestamp)
+    }
+}
+
+func getMeetingInvitation() string {
+    return "Ребята, пойдем соберёмся! (с)"
+}
